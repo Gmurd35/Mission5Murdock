d28db8242ca22a870489073ff9a7ede911c74098
Gmurd35 <119529326+Gmurd35@users.noreply.github.com>
2024-02-16 08:42:20 -0700

    Add files via upload

---
+++ wwwroot/js/site.js
@@ -0,0 +1,24 @@
+﻿// calculator.js
+
+//This takes the users inputs and calculates the total cost 
+$(document).ready(function () {
+    //On button click it does the calculation, and prevents it from refreshing until the next button click
+    $('#calculateBtn').click(function (event) {
+        event.preventDefault(); // Prevent form submission
+
+        var hours = parseFloat($('#hours').val());
+
+        // Make sure input is a positive number
+        if (isNaN(hours) || hours <= 0) {
+            alert('Please enter a valid positive number of hours.');
+            return;
+        }
+        //Hourly rate
+        var rate = parseFloat($('#rate').val().replace('$', '')); 
+        //Total calculation
+        var total = hours * rate;
+
+        //Display the total in the box
+        $('#total').val('$' + total.toFixed(2));
+    });
+});
